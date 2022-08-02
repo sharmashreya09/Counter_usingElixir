e2f3bf7c58feaa060309ade5d0a497611fecbc71
sharmashreya09 <sharmashreya092000@gmail.com>
2022-08-02 07:31:14 -0700

    Project done

---
+++ config/test.exs
@@ -0,0 +1,14 @@
+import Config
+
+# We don't run a server during test. If one is required,
+# you can enable the server option below.
+config :counter, CounterWeb.Endpoint,
+  http: [ip: {127, 0, 0, 1}, port: 4002],
+  secret_key_base: "LS3oJr16cEJaoF7iU108+6x9rexOZv+l0wzPPwnR8e39sZEniyHqJC3PHwqEDsfn",
+  server: false
+
+# Print only warnings and errors during test
+config :logger, level: :warn
+
+# Initialize plugs at runtime for faster test compilation
+config :phoenix, :plug_init_mode, :runtime
